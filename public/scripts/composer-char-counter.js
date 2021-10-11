2a9e37affd50b240b375f3182e2c4c1fa06563ac
YBYuan007 <yuanyanbin2011@gmail.com>
2021-10-11 17:20:50 +0000

    added the counter change

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,14 +1,18 @@
 // function to count characters in the tweet
 
+// const { text } = require("body-parser");
+
 $(document).ready(function() {
   $('#tweet-text').on('keyup', () => {
     let counter = 140;
     let a = $("#tweet-text").val().length;
     let b = counter - a;
-  
+    $(".counter").text(b); 
+
     if (b < 0) {
       $('#count-text').addClass("countOver");
     } else {
+      console.log("hello", b);
       $('#count-text').removeClass("countOver");
     }
   });
